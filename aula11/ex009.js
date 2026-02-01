1b3edc971063cac48d859e6acbcb74eb2851820d
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-01-31 23:09:49 -0300

    java script att

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
 var pais = 'brasil'
 console.log(`Vivendo em ${pais}`)
-if (pais == 'brasil') { //condição 
+if (pais == 'brasil') { //condição composta
     console.log('brasileiro')
 }else{
     console.log('estrangeiro')
