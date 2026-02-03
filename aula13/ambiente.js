a27b481a860f282e02cf84ad8f620aa35ab41076
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-03 20:58:00 -0300

    JavaScript ex

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*var c = 1
+do{  
+    console.log(`passo ${c}`)
+    c++
+} while( c <= 6)
+ */
+
+
+
+
+/*
+var c = 1
+while (c <= 4){v  
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+var teste = 1
+while (teste <= 10){
+    console.log('comi uma fatia de pizza!')
+    teste++
+}
