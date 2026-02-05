2db5fa377e6bf4134170ec33530773164d5baf83
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-05 20:37:37 -0300

    ex js att

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,18 @@
 function tabuada(){
-    
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    let n = Number(num.value)
+    if (num.value.length == 0){
+        window.alert('Porfavor, escolha outro numero!')
+    } else {
+        let c = 1 
+        tab.innerHTML = ''
+        while (n <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n * c}`
+            item.value = `tab${c}` 
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
