c472bf5e09ebad3161d2be1e9deb65ef5980a03e
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-08 11:25:14 -0300

    ex javaScript

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,14 +1,11 @@
 let numeros = document.getElementById('txtn')
 let lista = document.getElementById('numadd')
 let valores = []
-function isnumero(n){
-    isnumero(n)
-}
 
 function numero(){
     let n = Number(numeros.value)
     if (n <= 100 && n > 0) {
-        window.alert('Tudo certo!')
+        
     } else {
         window.alert('Erro')
     }
