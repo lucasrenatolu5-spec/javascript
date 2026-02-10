048e4d984a409be2e2b588d77e8411fff48236e9
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-10 19:50:34 -0300

    js ex

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -2,11 +2,25 @@ let numeros = document.getElementById('txtn')
 let lista = document.getElementById('numadd')
 let valores = []
 
-function numero(){
-    let n = Number(numeros.value)
+function jaExiste(n, valores){
+    for(let i = 1;i < valores.length; i++){
+        if(valores.indexOf(n) == i ){
+           window.alert('Numero invalido, ou ja se encontra na lista')
+        }
+    }
+}
+jaExiste(valores = Number(n.value))
+
+//function pra adicionar numero na lista
+function numero(){ 
+    n = Number(numeros.value)
+    let item = document.createElement('option')
     if (n <= 100 && n > 0) {
-        
+        lista.appendChild(item)
+        valores.push(n)
+        item.text = `Adicionado ${n}`
+        item.value = n
     } else {
-        window.alert('Erro')
+        window.alert('Numero invalido, ou ja se encontra na lista')
     }
 }
