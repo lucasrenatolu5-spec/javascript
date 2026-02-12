667b6ed7a849409d202bd8bd54af956e3f4c0f85
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-12 17:32:04 -0300

    javaScript ex

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -7,14 +7,77 @@ function jaExiste(n, valores){
 }
 //function pra adicionar numero na lista
 function numero(){ 
+    let res = document.getElementById('res')
+
+    if(res.innerHTML != ""){
+        valores = []
+        lista.innerHTML = ""
+        res.innerHTML = ""
+    }
+
+
     let n = Number(numeros.value)
     if (n <= 100 && n > 0 && !jaExiste(n, valores)) {
         let item = document.createElement('option')
         lista.appendChild(item)
         valores.push(n)
-        item.text = `Adicionado ${n}`
+        item.text = `Valor ${n} adicionado.`
         item.value = n
     } else {
         window.alert('Numero invalido, ou ja se encontra na lista')
     }
+}
+
+function somarTudo(){
+    let soma = Number("")
+
+    for (let i = 0; i < valores.length; i++ ){
+        soma += Number(valores[i])
+    }
+    return soma
+}
+
+function finalizar(){
+    let maior = pegarMaior()
+    let menor = pegarMenor()
+    let res = document.getElementById('res') 
+    let total = somarTudo()   
+    let media = total / valores.length
+    let texto = `
+    Somando todos os valores, temos ${total}. <br><br>
+    Ao todo, temos ${valores.length} números cadastrados. <br><br>
+    A média dos valores digitados é ${media}. <br><br>
+    O maior valor encontrado foi ${maior}.
+    `
+    res.innerHTML = texto
+}
+
+function pegarMaior(){
+    if(valores.length == 0){
+        return null
+    }
+
+    let maior = valores[0]
+
+    for (let i = 1; i < valores.length; i++){
+        if(valores[i] > maior){
+            maior = valores[i]
+        }
+    }
+    return maior 
+}
+
+function pegarMenor(){
+    if(valores.length == 0){
+        return null
+    }
+
+    let menor = valores[0]
+
+    for(let i = 1;i > valores.length[i]; i++){
+        if(valores[i] < menor){
+            menor = valores[i]
+        }
+    }
+    return menor
 }
