5426a0f769296770b9448f836697098c3055f413
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-25 19:34:00 -0300

    Pratica concluida

--- pratica05/modelo/script.js
+++ pratica05/modelo/script.js
@@ -4,6 +4,18 @@ let Lista = []
 enviar.addEventListener('click', function(){
     let tarefa = document.getElementById('taref').value
     Lista.push(tarefa)
-    lista.innerHTML += `<li>Adicionado: ${tarefa}.</li>`
+    const li = document.createElement('li')
+    li.textContent = tarefa
+    lista.appendChild(li)
+    const btn = document.createElement('button')
+    btn.textContent = 'X'
+    li.appendChild(btn)
+    btn.style.marginLeft = '10px'
+    li.style.marginTop = '10px'
+    // remove element for list!
+btn.addEventListener('click', function(){
+li.remove()
+})
+
 })
 
