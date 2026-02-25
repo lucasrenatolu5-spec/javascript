1e0e7bbf2dd2a41602dee27df94a4a8cdb5df3f0
lucasrenatolu5-spec <lucasrenatolu5@gmail.com>
2026-02-25 20:06:36 -0300

    pratica completa

--- pratica05/modelo/script.js
+++ pratica05/modelo/script.js
@@ -3,19 +3,36 @@ let lista = document.getElementById('lista')
 let Lista = []
 enviar.addEventListener('click', function(){
     let tarefa = document.getElementById('taref').value
+    if(tarefa == ""){
+        alert('Adicione alguma tarefa, por favor!')
+    } else{
     Lista.push(tarefa)
     const li = document.createElement('li')
     li.textContent = tarefa
     lista.appendChild(li)
     const btn = document.createElement('button')
+
     btn.textContent = 'X'
     li.appendChild(btn)
+
     btn.style.marginLeft = '10px'
+    btn.style.marginRight = '10px'
     li.style.marginTop = '10px'
-    // remove element for list!
+
+    const add = document.createElement('button')
+    add.textContent = 'Feito'
+    li.appendChild(add)
+
+add.addEventListener('click', function(){
+    add.style.background = '#25684e'
+    add.style.color = '#fff'
+}) 
+
 btn.addEventListener('click', function(){
-li.remove()
+   li.remove()
+
 })
+    }
 
 })
 
